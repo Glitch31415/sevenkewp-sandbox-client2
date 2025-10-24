a0d01e450e64338fb7b90e9d0e57d444a5eea28d
Glitch31415 <123266631+Glitch31415@users.noreply.github.com>
2025-10-24 01:33:40 -0700

    test

--- cl_dll/input.cpp
+++ cl_dll/input.cpp
@@ -66,10 +66,10 @@ cvar_t	*cl_pitchspeed;
 cvar_t	*cl_anglespeedkey;
 cvar_t	*cl_vsmoothing;
 
-cvar_t *cl_waah;
+cvar_t *cl_waah = gEngfuncs.pfnRegisterVariable ( "cl_waah", "0", FCVAR_ARCHIVE );
 
 
-cl_waah		= gEngfuncs.pfnRegisterVariable ( "cl_waah", "0", FCVAR_ARCHIVE );
+//cl_waah		= gEngfuncs.pfnRegisterVariable ( "cl_waah", "0", FCVAR_ARCHIVE );
 //= CVAR_CREATE("cl_waah", "0", 0);
 
 /*
